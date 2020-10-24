a3e27589ed40882137308cf7fcdc1ba1900246d9
goldin2008 <goldin20082011@gmail.com>
2020-10-24 09:27:37 -0400

    Update.

---
+++ cloud-c3-microservices/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=udacitycloud
+export POSTGRES_PASSWORD=yl110119
+export POSTGRES_HOST=database-1.cl3uuqeap3d0.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=udacity-cloud-developer
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
